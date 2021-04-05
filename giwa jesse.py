1c01bf78db502184fe3c1ce0ab2401c9eb92ed4a
jaysay09 <81833961+jaysay09@users.noreply.github.com>
2021-04-05 23:15:07 +0100

    csc 104 assignment

---
+++ giwa jesse.py
@@ -0,0 +1,43 @@
+
+-assignment num 1-
+
+print("name= giwa jesse")
+print("department= science education")
+
+-assignmecnt num 2-
+
+scare = "boo!"
+print(scare)
+
+-assignment num 3-
+
+character_two_vairable_a = "12"
+character_two_vairable_b = "8" 
+character_name = "bola"
+print("character_two_vairable_a + character_two_vairable_b = "20" ")
+print("bola is 20 years old")
+
+-assignment num 4-
+
+rem = 8 / 0.5
+print(rem)
+
+-assignment num 5-
+
+character_two_number = 16 + 24
+print(character_two_number)
+
+-assignment num 6-
+
+print(8 % 3)
+
+-assignment num 7-
+
+character_multiply = 2 * 4
+character_add = 4 + 2
+character_result =(2 * 4 * (4 + 2))
+print(character_result)
+
+
+
+
